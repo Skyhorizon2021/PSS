b1881339b0c187f5037644f78895ef75458424c3
NinjaTaco51 <113728233+NinjaTaco51@users.noreply.github.com>
2024-04-28 22:47:28 -0700

    Starting
    Starting our project classes

---
+++ PSS System/PSS.py
@@ -0,0 +1,26 @@
+class PSS:
+    def createTask():
+        
+    def viewTask():
+        
+    def deleteTask():
+
+    def editTask():
+
+    def writeToFile():
+        
+    def readFromFile():
+
+    def viewDaySchedule():
+
+    def viewWeekSchedule():
+
+    def viewMonthSchedule():
+
+    def writeDaySchedule():
+
+    def writeWeekSchedule():
+
+    def writeMonthSchedule():
+
+# Just written down the required methods from our PSS diagrams
