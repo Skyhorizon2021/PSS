a4d053300c6f90a26d18e5ae7f1fdab585942b9d
SkyHorizon2021 <locng2021@gmail.com>
2024-04-30 15:48:03 -0700

    renamed folder

--- PSS System/AntiTask.py
+++
@@ -1,4 +0,0 @@
-class Anti(Task):
-    def __init__(self, _name, _start, _duration, _date, _type):
-        super.__init__(_name, _start, _duration, _date)
-        self.type = _type
--- PSS System/Recurring.py
+++
@@ -1,7 +0,0 @@
-class Recurring(Task):
-    def __init__(self, _name, _start, _duration, _date, _type, _startDate, _endDate, _frequency):
-        super.__init__(_name, _start, _duration, _date)
-        self.type = _type
-        self.startDate = _startDate
-        self.endDate = _endDate
-        self.frequency = _frequency
