dd7a2af66ce1be7d39dafc2ea8ced23163ffa6be
NinjaTaco51 <113728233+NinjaTaco51@users.noreply.github.com>
2024-05-03 21:27:38 -0700

    Task and Viewer
    (PSS updates)
    Added return objects for all tasks when creating
    
    (Viewer Update)
    Added Schedule json file loading (feature not confirmed)
    
    (Both)
    Added 'pass' just to remove errors
    
    Fixed switch statements

--- PSS_System/PSS.py
+++ PSS_System/PSS.py
@@ -2,9 +2,10 @@ class PSS:
 
     def createTask():
         print("Select the type of task:\n1. Recurring\n2. Transcient\n3. Recurring")
-        selecttion = input(int())
-        switch(type){
+        selection = input(int())
+        match selection:
             case 1:
+                # Entering parameters
                 name = input("Enter the name of the task: ")
                 taskType = "Recurring Task"
                 start = input(float("Enter the task's start time: "))
@@ -12,45 +13,56 @@ class PSS:
                 startDate = input(float("Enter the task's start date: "))
                 endDate = input(int("Enter the task's end date: "))
                 frequency = input(int("Enter the task's frequency: "))
-                break;
+                
+                # Creating recurring task
+                reTask = Recurring(name, start, duration, startDate, taskType,
+                endDate, frequency)
+                return reTask
             case 2:
+                # Entering parameters
                 name = input("Enter the name of the task: ")
                 taskType = "Transcient Task"
                 start = input(float("Enter the task's start time: "))
                 duration = input(float("Enter the task's duration: "))
                 date = input(int("Enter the task's date: "))
-                break;
+
+                # Creating transient task
+                trTask = Transient(name, start, duration, date, taskType)
+                return trTask
             case 3:
+                # Entering parameters
                 name = input("Enter the name of the task: ")
                 taskType = "Anti Task"
                 start = input(float("Enter the task's start time: "))
                 duration = input(float("Enter the task's duration: "))
                 date = input(int("Enter the task's date: "))
-                break;
-        }
+
+                # Creating anti task
+                anTask = AntiTask(name, start, duration, date, taskType)
+                return anTask
 
 
     def viewTask():
-        print()
+        pass
         
     def deleteTask():
-
+        pass
     def editTask():
-
+        pass
     def writeToFile():
-        
+        pass
     def readFromFile():
-
+        pass
     def viewDaySchedule():
-
+        pass
     def viewWeekSchedule():
-
+        pass
     def viewMonthSchedule():
-
+        pass
     def writeDaySchedule():
-
+        pass
     def writeWeekSchedule():
-
+        pass
     def writeMonthSchedule():
-
+        pass
 # Just written down the required methods from our PSS diagrams
--- PSS_System/view.py
+++ PSS_System/view.py
@@ -1,31 +1,39 @@
+import json
+
 def menu():
+    # Asks if they want to load a schedule
+    loadsch = input("Do you have a schedule to load?").lower()
+    if loadsch[0] == 'y':
+        while True:
+            filename = input("Enter file name:\n(e.g. \'sample.json\')")
+            # Loads the json file into as a dictionary named 'schedule'
+            try:
+                jsonfile = open(filename)
+                schedule = json.load(jsonfile)
+                break
+            except FileNotFoundError:
+                print("There is no file of that name.\nPlease try again...")
+
     print("1. Tasks\n2. Schedule\n3. End Program")
     userinput = input(int("Select an option:"))
 
-    switch(userinput){
+    match userinput:
         case 1:
-            print("1. Create\n2. Delete\n3. Edit\n4. View")
+            print("\n1. Create\n2. Delete\n3. Edit\n4. View")
             choice = input(int("Option: "))
             # Insert nested switch state to account for all task modification options
-            break;
         case 2:
             print("1. View Schedule\n2. Write Schedule")
             choice = input(int("Option: "))
-            switch(choice){
+            match choice:
                 case 1:
                     print("1. View Day Schedule\n2. View Week Schedule\n3. View Month Schedule ")
                     viewer = input(int("Option: "))
                     # Take in date and display scheduled option
-                    break;
+                    
                 case 2: 
                     print("1. Write Day Schedule\n2. Write Week Schedule\n3. Write Month Schedule ")
                     viewer = input(int("Option: "))
-                    break;
-                default:
-                    break;
-            }
-            break;
+                    
         case 3:
-
-            break;
-    }
+            continue
